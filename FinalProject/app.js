a6db5656d52f31ba6fec380ef2b40f0c0b221af1
rconc016 <rconc016@fiu.edu>
2017-11-29 00:54:42 -0500

    Updated index page title

--- FinalProject/app.js
+++ FinalProject/app.js
@@ -4,7 +4,7 @@ var app = express();
 app.set('view engine', 'pug')
 
 app.get('/', function (req, res) {
-   res.render('index', { title: 'Hey', message: 'Hello there!' })
+   res.render('index', { title: 'rconc016 - COP4710' })
 })
 
 var server = app.listen(8080, '127.0.0.1', function () {
