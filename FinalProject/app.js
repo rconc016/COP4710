2fc4131710656c9fcaf70daab6f9c927744d181b
rconc016 <rconc016@fiu.edu>
2017-11-29 01:05:08 -0500

    Index page now has links to display_table page

--- FinalProject/app.js
+++ FinalProject/app.js
@@ -1,15 +1,33 @@
-var express = require('express');
-var app = express();
+var express = require('express')
+var app = express()
+var mysql = require('mysql')
 
 app.set('view engine', 'pug')
 
-app.get('/', function (req, res) {
-   res.render('index', { title: 'rconc016 - COP4710' })
+var db_conn = mysql.createConnection(
+{
+	host: 'localhost',
+	user: 'root',
+	password: 'root'
 })
 
-var server = app.listen(8080, '127.0.0.1', function () {
-   var host = server.address().address
-   var port = server.address().port
-   
-   console.log("Example app listening at http://%s:%s", host, port)
+app.get('/', function (req, res) 
+{
+	tables = ['author', 'book', 'copy', 'publisher']
+
+	res.render('index', { title: 'rconc016 - COP4710', tables: tables })
+})
+
+db_conn.connect(function(err) 
+{
+	if (err) throw err
+	console.log("Connected to the MySQL database!")
+
+	var server = app.listen(8080, '127.0.0.1', function () 
+	{
+	   var host = server.address().address
+	   var port = server.address().port
+	   
+	   console.log("Example app listening at http://%s:%s", host, port)
+	})
 })
