7fb674a8ff9a5348217d13aa5deabffb2cc3b6dc
rconc016 <rconc016@fiu.edu>
2017-11-29 00:49:21 -0500

    Server now uses Pug to render pages

--- FinalProject/app.js
+++ FinalProject/app.js
@@ -1,8 +1,10 @@
 var express = require('express');
 var app = express();
 
+app.set('view engine', 'pug')
+
 app.get('/', function (req, res) {
-   res.send('Hello World');
+   res.render('index', { title: 'Hey', message: 'Hello there!' })
 })
 
 var server = app.listen(8080, '127.0.0.1', function () {
